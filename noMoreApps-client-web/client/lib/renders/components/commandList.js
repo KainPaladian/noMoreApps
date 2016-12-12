9b32ac3597a2941543cd40f203d877ea140070a5
gustavo.santos <gustavofelipesanto@gmail.com>
2016-12-12 01:56:43 -0200

    button group

--- noMoreApps-client-web/client/lib/renders/components/commandList.js
+++ noMoreApps-client-web/client/lib/renders/components/commandList.js
@@ -1,6 +1,6 @@
 processCommandList = function(container,componentInfo){
 	var options = componentInfo.options;
-	var mainElement = $("<div></div>").addClass("component-command-list");
+	var mainElement = $("<div></div>").addClass("component-command-list btn-group");
 
 	$(mainElement).uniqueId();
 	
